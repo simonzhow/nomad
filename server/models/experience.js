076700ab79ca5457de9c8e3f0c2ff25dd4fb50da
Anshul <agg_anshul@yahoo.com>
2017-11-04 19:23:39 -0700

    added id and description to schema

--- server/models/experience.js
+++ server/models/experience.js
@@ -3,6 +3,8 @@ const Schema = mongoose.Schema;
 
 const experienceSchema = new Schema({
   name: { type: String, required: true },
+  id: { type: Number, required: true },
+  description: { type: String, required: true },
   location: [{ type: Schema.Types.ObjectId, ref: 'Location' }],
   date_added: { type: 'Date', default: Date.now, required: true },
 });
