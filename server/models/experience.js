ed92505c100830241a59032b6aca17680bfe027f
Anshul <agg_anshul@yahoo.com>
2017-11-04 19:25:53 -0700

    don't need an ID, blame Uma

--- server/models/experience.js
+++ server/models/experience.js
@@ -3,7 +3,6 @@ const Schema = mongoose.Schema;
 
 const experienceSchema = new Schema({
   name: { type: String, required: true },
-  id: { type: Number, required: true },
   description: { type: String, required: true },
   location: [{ type: Schema.Types.ObjectId, ref: 'Location' }],
   date_added: { type: 'Date', default: Date.now, required: true },
