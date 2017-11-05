67ded588892129728c2d13e96959624063a0e873
Monil Patel <Monil@Monils-MacBook-Pro.local>
2017-11-04 19:28:58 -0700

    mp/userschema

---
+++ server/models/user.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  first_name: { type: 'String', required: true },
+  last_name: { type: 'String', required: true },
+  email: { type: 'String', required: true },
+  username: { type: 'String', required: true },
+  home: [{ type: Schema.Types.ObjectId, ref: 'Location' }],
+  points: { type: 'int', required: true },
+  travel_entry: [{ type: Schema.Types.ObjectId, ref: 'TravelEntry' }],
+  date_added: { type: 'Date', default: Date.now, required: true },
+});
+// User
+
+// Experience
+
+// TravelEntry
+
+
+
+export default mongoose.model('User', userSchema);
