5a15f37c41ca1a04c379c72bf9469a75085ad332
Anshul <agg_anshul@yahoo.com>
2017-11-04 19:19:32 -0700

    added schemas for location and for an experience

---
+++ server/models/experience.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const experienceSchema = new Schema({
+  name: { type: String, required: true },
+  location: [{ type: Schema.Types.ObjectId, ref: 'Location' }],
+  date_added: { type: 'Date', default: Date.now, required: true },
+});
+
+export default mongoose.model('Experience', experienceSchema);
