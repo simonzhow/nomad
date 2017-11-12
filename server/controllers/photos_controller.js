e0f3183ebd9350faaeaa4a09fdaf8a1f3d7db84a
Anshul <agg_anshul@yahoo.com>
2017-11-12 14:48:47 -0800

    fixed some styling

--- server/controllers/photos_controller.js
+++ server/controllers/photos_controller.js
@@ -1,11 +1,11 @@
-var cloudinary = require('cloudinary');
-import Photo from ('../models/Photo');
+import cloudinary = require('cloudinary');
+import Photo from '../models/Photo';
 
 /*
 *how to pass in photo upload to this point?
 */
 
-export function upload_photo(req, res){
+export function uploadPhoto(req, res){
 	cloudinary.uploader.upload('pizza.jpg', {tags: 'basic_sample', uid: 'uid'}, function(err, image) {
 		if(err){
 			console.warn(err);
