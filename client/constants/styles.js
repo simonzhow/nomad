898980c4e54c1bcf60ae74ac7a3f4fa903ace12a
Mihirmathur <mihirmathur@g.ucla.edu>
2017-12-01 01:22:03 -0800

    Some styles were missing in previous commit.

--- client/constants/styles.js
+++ client/constants/styles.js
@@ -6,10 +6,12 @@ const colors = {
   gray: '#777777',
   lightGray: '#D3D3D3',
   green: 'rgb(72, 112, 65)',
+  violetRed: 'palevioletred',
 }
 
 const shadows = {
   default: '0px 4px 10px rgba(0,0,0,0.25)',
+  cardshadow: '0 3px 4px 0 rgba(0,0,0,.14), 0 3px 3px -2px rgba(0,0,0,.2), 0 1px 8px 0 rgba(0,0,0,.12)',
 }
 
 // eslint-disable-next-line
