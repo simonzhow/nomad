b762a29c9bbedad1810e924e982968856fb34ec4
Bibek <bibekg@ucla.edu>
2017-12-14 13:29:38 -0800

    Documented auth methods

--- server/auth/facebook-token.js
+++ server/auth/facebook-token.js
@@ -4,24 +4,38 @@ import config from '../config'
 import User from '../models/user'
 import UserHelpers from './user_helpers'
 
+/**
+ * @callback passportStrategyDoneCallback
+ * @param {Error} error - Error object
+ * @param {Object} user - DB user object
+ */
+/**
+  Finds a user (or creates one if necessary) in our DB corresponding to the
+  provided FB profile and passes it to the done callback
+  @param {string} accessToken - Client's FB-provided access token
+  @param {string} refreshToken - Client's FB-provided refresh token
+  @param {Object} profile - FB profile object that contains a 'id' property we store in our DB
+  @param {passportStrategyDoneCallback} done - Callback that PassportJS requires to continue middleware execution
+*/
+const authProcessor = (accessToken, refreshToken, profile, done) => {
+  // Find a user in the local db using the profile.id
+  // If the user is found, return the user data using the done()
+  // If the user is not found, create one in the local db and return
+  User.findOne({ user_id: profile.id }).then(user => {
+    if (user) {
+      done(null, user)
+    } else {
+      UserHelpers
+        .createNewUser(profile)
+        .then(newUser => done(null, newUser))
+        .catch(error => {
+          // eslint-disable-next-line no-console
+          console.log('error', `Error when creating new user: ${error}`)
+        })
+    }
+  })
+}
+
 module.exports = () => {
-  const authProcessor = (accessToken, refreshToken, profile, done) => {
-    // Find a user in the local db using the profile.id
-    // If the user is found, return the user data using the done()
-    // If the user is not found, create one in the local db and return
-    User.findOne({ user_id: profile.id }).then(user => {
-      if (user) {
-        done(null, user)
-      } else {
-        UserHelpers
-          .createNewUser(profile)
-          .then(newUser => done(null, newUser))
-          .catch(error => {
-            // eslint-disable-next-line no-console
-            console.log('error', `Error when creating new user: ${error}`)
-          })
-      }
-    })
-  }
   passport.use(new FacebookTokenStrategy(config.fb, authProcessor))
 }
--- server/util/file-helpers.js
+++ server/util/file-helpers.js
@@ -1,19 +1,13 @@
 import cloudinary from 'cloudinary'
 import multer from 'multer'
 /**
-<<<<<<< HEAD
-  * Handles uploading to Cloudinary
-  * @param req {JSON} Request send to function
-  * @param res {JSON} Response sent from function
-=======
-   Handles uploading to Cloudinary
-  * @param req {JSON}
-  * @param res {JSON}
->>>>>>> master
+  * Express middleware that attaches a Cloudinary url to req.body if req.file
+  * is present
+  * @function
+  * @param {Object} req - Express Request object
+  * @param {Object} res - Express Response object
   */
 export const attachPhotoUrl = (req, res, next) => {
-  // If a file (photo) is provided with the request, attach a Cloudinary url
-  // to the request before passing it forward
   if (req.file) {
     cloudinary.uploader.upload(req.file.path, (result) => {
       req.body.photoUrl = result.url
